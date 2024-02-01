ed729eb1e840bf5caad7764640d742ba162c96a7
Tony Li <tingfung.tony@gmail.com>
2024-02-02 01:30:49 +0800

    chore: add env

--- env/server.mjs
+++ env/server.mjs
@@ -3,16 +3,22 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
 export const env = createEnv({
-    server: {
-        NEXTAUTH_SECRET: z.string(),
-        GOOGLE_ID: z.string(),
-        GOOGLE_SECRET: z.string(),
-        DATABASE_URL: z.string(),
-    },
-    runtimeEnv: {
-        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-        GOOGLE_ID: process.env.GOOGLE_ID,
-        GOOGLE_SECRET: process.env.GOOGLE_SECRET,
-        DATABASE_URL: process.env.DATABASE_URL,
-    },
+  server: {
+    NEXTAUTH_SECRET: z.string(),
+    GOOGLE_ID: z.string(),
+    GOOGLE_SECRET: z.string(),
+    DATABASE_URL: z.string(),
+    DIRECT_URL: z.string().optional(),
+  },
+  client: {
+    NEXT_PUBLIC_NEXTAUTH_URL: z.string().optional(),
+  },
+  runtimeEnv: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXT_PUBLIC_NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
+  },
 });
